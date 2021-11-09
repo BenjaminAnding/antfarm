760fad4bac98dfba387d07e5657e0911f511992c
Ben Anding <benanding@Bens-MacBook-Air.local>
2021-11-08 21:39:36 -0600

    implemented some simple behaviors

--- antfarm.py
+++ antfarm.py
@@ -69,10 +69,14 @@ class Farm:
 	def getCell(self, x, y):
 		return self.farm[y][x]
 
+	def equalize(self, x, y):
+		self.getCell(x, y).incPosChem(-1*self.getCell(x,y).getNegChem())
+		self.getCell(x, y).incNegChem(-1*self.getCell(x,y).getNegChem())
+
 	def drawFarm(self):
 		for i in range(self.yDim):
 			for j in range(self.xDim):
-				self.getCell(j,i).incPosChem(-.02)
+				self.equalize(j,i)
 				self.rowString+=getattr(self, self.getCell(j,i).getContents()[-1])
 			print(self.rowString)
 			self.rowString = ''	
@@ -84,14 +88,19 @@ class Ant:
 	- Requires a farm and 3 integer values to construct (id, x, y). 
 	- These are the little buggers we're going to observe.
 	"""
-	directions = ['up', 'down', 'left', 'right']
+	directions = ['wait', 'up', 'down', 'left', 'right']
+	dirmap = {'wait':(0, 0), 'up':(0, -1), 'down':(0, 1), 'left':(-1, 0), 'right':(1, 0)}
 	home = None
 	antId = None
 	xPos = None
 	yPos = None
+	tendencies = ['courageous', 'cautious', 'caring']
 	def __init__(self, farm, antId, xPos, yPos):
+		self.tendency = self.tendencies[random.randint(0,2)]
 		self.home = farm
 		self.antId = antId
+		self.isHappy = None
+		self.isOnFood = None
 		self.xPos = int(xPos)
 		self.yPos = int(yPos)
 
@@ -111,14 +120,14 @@ class Ant:
 	def senseDir(self):
 		posChemTrails = {}
 		negChemTrails = {}
-		directions = {'up':(0, -1), 'down':(0, 1), 'left':(-1, 0), 'right':(1, 0)}
-		for direction in directions:
-			posChemTrails[direction] = self.home.getCell((self.getXPos()+directions[direction][0])%self.home.xDim, (self.getYPos()+directions[direction][1])%self.home.yDim).getPosChem()
-			negChemTrails[direction] = self.home.getCell((self.getXPos()+directions[direction][0])%self.home.xDim, (self.getYPos()+directions[direction][1])%self.home.yDim).getNegChem()
-		return max(posChemTrails, key = (lambda key: posChemTrails[key]))
+		#directions = {'wait':(0,0), 'up':(0, -1), 'down':(0, 1), 'left':(-1, 0), 'right':(1, 0)}
+		for direction in self.dirmap:
+			posChemTrails[direction] = self.home.getCell((self.getXPos()+self.dirmap[direction][0])%self.home.xDim, (self.getYPos()+self.dirmap[direction][1])%self.home.yDim).getPosChem()
+			negChemTrails[direction] = self.home.getCell((self.getXPos()+self.dirmap[direction][0])%self.home.xDim, (self.getYPos()+self.dirmap[direction][1])%self.home.yDim).getNegChem()
+		return max(posChemTrails, key = (lambda key: posChemTrails[key])), min(posChemTrails, key = (lambda key: posChemTrails[key]))
 
 	def moveSensible(self):
-		self.move(self.senseDir())
+		self.move(self.senseDir()[0])
 
 	def move(self, direction):
 		if (direction == 'left'):
@@ -131,18 +140,58 @@ class Ant:
 			self.setYPos((self.getYPos() + 1) % self.home.yDim)
 
 	def mover(self):		
-		randdir = self.directions[random.randint(0,100) % len(self.directions)]
+		randdir = self.directions[random.randint(0,99) % len(self.directions)]
 		self.move(randdir)
 	
+	def mood(self):
+		challenger = random.randint(1,3) % 2
+		if (self.tendencies.index(self.tendency) + 1) % 2 >= challenger:
+			self.tendency = self.tendencies[challenger+1]
+		else:
+			self.tendency = self.tendencies[challenger]
+
+	def move_cautious(self):
+		self.move(self.senseDir()[0])
+
+	def move_curious(self):
+		self.move(self.senseDir()[1])
+
+	def move_caring(self):
+		self.move('wait')
+			
+	def cautious_sanity(self):
+		neighbors = 0
+		for direction in self.dirmap:
+			if 'ant' in self.home.getCell((self.getXPos()+self.dirmap[direction][0])%self.home.xDim, (self.getYPos()+self.dirmap[direction][1])%self.home.yDim).getContents():
+				neighbors += 1
+		return neighbors < 2
+
+	def curious_sanity(self):
+		neighbors = 0
+		for direction in self.dirmap:
+			if 'ant' in self.home.getCell((self.getXPos()+self.dirmap[direction][0])%self.home.xDim, (self.getYPos()+self.dirmap[direction][1])%self.home.yDim).getContents():
+				neighbors += 1
+		return neighbors < 3
+	
+	def caring_sanity(self):
+		neighbors = 0
+		for direction in self.dirmap:
+			if 'ant' in self.home.getCell((self.getXPos()+self.dirmap[direction][0])%self.home.xDim, (self.getYPos()+self.dirmap[direction][1])%self.home.yDim).getContents():
+				neighbors += 1
+		return neighbors < 3
+
+
 	def live(self):
+		self.mood()
+		getattr(self, self.tendency+'_sanity')()	
 		self.home.getCell(self.getXPos(), self.getYPos()).incPosChem(.1)
-		if ((random.randint(0,100) % 3) == 0):
-			self.moveSensible()
+		if ((random.randint(0,99) % 3) == 0):
+			getattr(self, 'move_'+self.tendency)()
 		else:
 			self.mover()
 
 	def toString(self):
-		return 'Ant:'+str(self.antId)+' at '+str(self.xPos)+','+str(self.yPos)
+		return self.tendency+' Ant:'+str(self.antId)+' at '+str(self.xPos)+','+str(self.yPos)
 
 class Shell:
 	"""
@@ -152,12 +201,12 @@ class Shell:
 	"""
 	ants = {}
 	ants[0] = 'dummy'
-	commands = ['help', 'spawn', 'kill', 'list', 'step']
+	commands = ['help', 'spawn', 'kill', 'list', 'sim']
 	commandHelp = {'help':'Displays help messages',
 					'spawn':'<n> spawn <x y> \n Spawn (n number of) ant(s) at (x y)|0 0',
 					'kill':'kill n <x> \n Kill ant with id n, if x is given kill all ants within id range [n,x]',
 					'list':'list \n Lists current living ants',
-					'step':'<n> step <x> \n Perform x steps of life simulation (n times)'}
+					'sim':'<n> sim <x> \n Perform x steps of life simulation (n times)'}
 	farm = None
 	def draw(self):
 		self.farm.drawFarm()
@@ -204,7 +253,7 @@ class Shell:
 			self.ants[max(self.ants.keys())+1] = Ant(self.farm, max(self.ants.keys())+1, 0, 0)
 		self.populate()
 
-	def step(self, args):
+	def sim(self, args):
 		if (len(args) == 1):
 			if not all(x.isdigit() for x in ''.join(args)):
 				print("'step' parameter must be a valid integer.")
@@ -246,21 +295,25 @@ class Shell:
 			for i in range(numTimes):
 				getattr(self, command.split(' ')[idx])(command.split(' ')[idx+1:])
 
-	def __init__(self):
-		print("Input x dim: ")
-		print(">", end='')
-		xDim = input()
-		while (not all(x.isdigit() for x in xDim)) or xDim is '':
-			print("Please enter valid integer: ")
+	def __init__(self, boo):
+		if not boo:
+			print("Input x dim: ")
 			print(">", end='')
 			xDim = input()
-		print("Input y dim: ")
-		print(">", end='')
-		yDim = input()
-		while (not all(x.isdigit() for x in yDim)) or yDim is '':
-			print("Please enter valid integer: ")
+			while (not all(x.isdigit() for x in xDim)) or xDim is '':
+				print("Please enter valid integer: ")
+				print(">", end='')
+				xDim = input()
+			print("Input y dim: ")
 			print(">", end='')
 			yDim = input()
+			while (not all(x.isdigit() for x in yDim)) or yDim is '':
+				print("Please enter valid integer: ")
+				print(">", end='')
+				yDim = input()
+		else:
+			xDim = 118
+			yDim = 22
 		print("Building farm...")
 		self.farm = Farm(int(xDim), int(yDim))
 		self.farm.drawFarm()
@@ -268,7 +321,14 @@ class Shell:
 if __name__ == '__main__':
 	print('Welcome to antfarm! Performing shell setup...')
 	print("First things first, let's make your farm!")
-	shell = Shell()
+	print('Default settings? 118 x 22 [Y/n]', end='')
+	command = input()
+	while command.strip().lower() not in ['', 'n', 'y']:
+		command = input()
+	if command.strip().lower() in ['', 'y']:
+		shell = Shell(True)
+	else:
+		shell = Shell(False)
 	command = ''
 	print('Type "help" for help.')
 	while (command != '**'):
